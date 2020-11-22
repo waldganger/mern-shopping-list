df6bfa2c5e331c595ad6499942faf1b09bd65243
waldganger <antony.merle@yahoo.fr>
2020-11-22 22:32:12 +0100

    fin ep3 reactstrap

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import AppNavBar from './components/AppNavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar />
+    </div>
+  );
+}
+
+export default App;
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,38 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Item Model
-const Item = require('../../models/Item');
+const Item = require("../../models/Item");
 
 // @route GET api/items
 // @desc Get all items
 // @access Public
 // Puisqu'on utilise le router d'express, pas besoin de spécifier la route complète '/api/items'
 // cette route est déjà spécifiée dans server.js, qui renvoit sur ce fichier. Donc on est dans '/'
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Item.find()
-  .sort({date: -1})       // -1 classement descendant
-  .then(items => res.json(items))
+    .sort({ date: -1 }) // -1 classement descendant
+    .then((items) => res.json(items));
 });
 
 // @route   POST api/items
 // @desc    Create an item
 // @access  Public
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newItem = new Item({
-    name: req.body.name
-  })
-  newItem.save().then(item => res.json(item));
-})
+    name: req.body.name,
+  });
+  newItem.save().then((item) => res.json(item));
+});
 
 // @route   DELETE api/items/:id
 // @desc    Delete an Item
 // @access  Public
-router.delete('/:id', (req, res) => {       // :id === placeholder for the id we'll pass
-  Item.findById(req.params.id)              // findById vient de Mongoose et fetch l'info de l'uri
-  .then(item => item.remove().then(() => res.json({delete_success: true})))
-  .catch(err => res.status(404).json({delete_success: false}));
-})
-  
+router.delete("/:id", (req, res) => {
+  // :id === placeholder for the id we'll pass
+  Item.findById(req.params.id) // findById vient de Mongoose et fetch l'info de l'uri
+    .then((item) =>
+      item.remove().then(() => res.json({ delete_success: true }))
+    )
+    .catch((err) => res.status(404).json({ delete_success: false, message: err }));
+});
 
 module.exports = router;
