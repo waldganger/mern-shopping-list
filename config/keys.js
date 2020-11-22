dd16d9274de28646a5a3145589e1c64d9c41daf4
waldganger <antony.merle@yahoo.fr>
2020-11-22 16:02:35 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI : 'mongodb+srv://antony:jIXwXb1TB503sPyf@cluster0.lainx.mongodb.net/mern_shopping?retryWrites=true&w=majority' 
+}
---
+++ routes/api/items.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get all items
+// @access Public
+// Puisqu'on utilise le router d'express, pas besoin de spécifier la route complète '/api/items'
+// cette route est déjà spécifiée dans server.js, qui renvoit sur ce fichier. Donc on est dans '/'
+router.get('/', (req, res) => {
+  Item.find()
+  .sort({date: -1})       // -1 classement descendant
+  .then(items => res.json(items))
+});
+
+// @route   POST api/items
+// @desc    Create an item
+// @access  Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  })
+  newItem.save().then(item => res.json(item));
+})
+
+// @route   DELETE api/items/:id
+// @desc    Delete an Item
+// @access  Public
+router.delete('/:id', (req, res) => {       // :id === placeholder for the id we'll pass
+  Item.findById(req.params.id)              // findById vient de Mongoose et fetch l'info de l'uri
+  .then(item => item.remove().then(() => res.json({delete_success: true})))
+  .catch(err => res.status(404).json({delete_success: false}));
+})
+  
+
+module.exports = router;
