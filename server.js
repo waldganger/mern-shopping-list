06cfc4c90b9f37a4025a36bacc3790648138632a
waldganger <antony.merle@yahoo.fr>
2020-12-03 19:05:03 +0100

    ajout route par défaut si autre endpoint que /api/items atteint

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const items = require('./routes/api/items');
+const bodyParser = require("body-parser");
+const path = require("path");
+const items = require("./routes/api/items");
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(express.json());
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -17,10 +18,19 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
-
 // Use Routes
-app.use('/api/items', items);
+app.use("/api/items", items);
 
+// Serve static assets (build folder) if in production
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
